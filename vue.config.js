ea8b24d39bc45bd70817e7df2c7810cc3eb783cc
suixian.gong <suixian.gong@sumscope.com>
2021-08-10 09:26:05 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+
+const vueConfig = {
+  publicPath: '/',
+  outputDir: 'dist',
+  devServer: {
+    disableHostCheck: true,
+    host: '127.0.0.1',
+    port: 7008,
+    open: true,
+    proxy: {
+      '/api/my/': {
+        target: 'http://172.16.97.194:3000',
+        changeOrigin: true,
+      }
+    },
+    watchOptions: {
+      ignored: ['**/node_modules', '**/.#*']
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+}
+
+module.exports = vueConfig
